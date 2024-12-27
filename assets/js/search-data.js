d7ee2360edf634f8696b6896c8fb623103e3f7b5
EyupBunlu <68071466+EyupBunlu@users.noreply.github.com>
2024-12-27 04:17:53 +0000

    Deploying to gh-pages from @ EyupBunlu/EyupBunlu.github.io@9a8e45fc225730541140f2aa23069237b7d73d00 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -112,9 +112,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-quantum-machine-learning-for-high-energy-physics",
+          title: 'Quantum Machine Learning for High Energy Physics',
+          description: "Google Summer of Code 2023 @ Machine Learning for Science",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
